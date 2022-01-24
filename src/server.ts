40d392f02ebf2edd8693d139042706c1db415a07
Rafael Morales <rmorales@utem.cl>
2022-01-24 19:17:09 -0300

    Installed packages, made first migrations

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
-import express, { Express, Request, Response } from 'express';
+import express, {Express, Request, Response} from 'express';
 
 const app: Express = express();
 
 // parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({extended: false}));
 
 // parse application/json
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.write('Hello!');
-    res.statusCode = 200;
+  res.write('Hello!');
+  res.statusCode = 200;
 });
 
 app.post('/login', (req: Request, res: Response) => {
-    const username: string = req.body.username;
-    const password: string = req.body.password;
+  const username: string = req.body.username;
+  const password: string = req.body.password;
 
-    res.send(`Username: ${username}\tPassword:${password}`);
+  res.send(`Username: ${username}\tPassword:${password}`);
 });
 
 app.listen(3000, () => console.log('Listening on Port 3000'));
