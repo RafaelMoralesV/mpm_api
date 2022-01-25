b2c5450f394aef683b755837a307af16c3d50f8a
Rafael Morales <rmorales@utem.cl>
2022-01-24 23:18:17 -0300

    Second migration. CardId should have scryfall's ID

---
+++ migrations/20220125015108-create-collection-entry.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('CollectionEntries', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      cardId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('CollectionEntries');
+  },
+};
---
+++ models/collectionentry.js
@@ -0,0 +1,26 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable valid-jsdoc */
+'use strict';
+const {
+  Model,
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class CollectionEntry extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  CollectionEntry.init({
+    user_id: DataTypes.INTEGER,
+    card_id: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'CollectionEntry',
+  });
+  return CollectionEntry;
+};
