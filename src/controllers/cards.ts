454c047ed033847f960249a5714297f33b61349d
Rafael Morales <rmorales@utem.cl>
2022-03-14 13:10:00 -0300

    Buscar coleccion por ID de Usuario

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -6,9 +6,23 @@ import CardAttributes from '../interfaces/card';
 // eslint-disable-next-line new-cap
 const router: Router = express.Router();
 
+// GET / - Obtiene una lista de todas las cartas.
 router.get('/', async (req: Request, res: Response) => {
   const collection: Array<CardAttributes> =
         await db.Card.findAll({user_id: req.user});
+  return res.json({
+    status: 200,
+    data: {
+      size: collection.length,
+      collection: collection,
+    },
+  });
+});
+
+router.get('/:userID', async (req: Request, res: Response) => {
+  const collection: Array<CardAttributes> =
+      await db.Card.findAll({user_id: req.query.user_id});
+
   return res.json({
     status: 200,
     data: {
